13f89fe8bbd848253cf89ab1707e62209f1a7638
Jos O'shea <jos@joshea.dev>
2023-03-02 14:55:02 -0500

    more cors testing

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,9 @@
 require_relative "boot"
 
 require "rails/all"
+require 'rack'
+require 'rack/cors'
+
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -11,6 +14,15 @@ module Lexcats
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+
+    config.middleware.insert_before 'ActionDispatch::Static', 'Rack::Cors' do
+      allow do
+        origins '*'
+        resource '*', headers: :any,
+                      methods: [:get, :post, :options, :delete, :head, :put],
+                      max_age: 0
+      end
+    end
     # Configuration for the application, engines, and railties goes here.
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
