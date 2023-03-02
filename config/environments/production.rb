2ebfdfcc85b07c74816e322b47a8bdd5e11e1f7c
Jos O'shea <jos@joshea.dev>
2023-03-02 17:23:00 -0500

    debugging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@ Rails.application.configure do
   config.eager_load = true
 
   # Full error reports are disabled and caching is turned on.
-  config.consider_all_requests_local       = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching = true
   config.action_dispatch.show_exceptions = true
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
