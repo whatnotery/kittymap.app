364dae5b6aed02e42bd0cf90c992717ba214f92d
Jos O'shea <jos@joshea.dev>
2023-03-02 17:14:58 -0500

    debugging

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -25,7 +25,7 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
-
+  config.action_dispatch.show_exceptions = true
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   # config.require_master_key = true
