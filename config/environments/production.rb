5949c08907973e92f615d175104f6d2f739f3fa3
Jos O'shea <jos@joshea.dev>
2023-03-02 21:19:56 -0500

    typo

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.action_mailer.default_url_options = { :host => "kittymap.app " }
+  config.action_mailer.default_url_options = { :host => "https://kittymap.app" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port: 587,
