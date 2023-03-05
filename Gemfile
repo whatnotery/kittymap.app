975f617b718533f379441e9348814bc55af030e3
Jos O'shea <jos@joshea.dev>
2023-03-05 17:27:06 -0500

    add geo tagging options

--- Gemfile
+++ Gemfile
@@ -38,6 +38,8 @@ gem "passwordless"
 gem 'cloudinary'
 gem "mapkick-rb"
 gem "rack-cors"
+gem "font-awesome-sass", "~> 6.3.0"
+
 # Use Kredis to get higher-level data types in Redis [https://github.com/rails/kredis]
 # gem "kredis"
 
--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,41 +1,77 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "mapkick/bundle"
-import {Turbo} from "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
 
 let lat
 let lon
+let geolocateButton
 let address
-let cat_address
-navigator.geolocation.getCurrentPosition((position) => {
+let timeout
+
+addEventListener("turbo:frame-load", (e) => {
+  let catLat = document.getElementById("cat_latitude")
+  let catLon = document.getElementById("cat_longitude")
+  let cat_address = document.getElementById('cat_address')
+  let geolocateButton = document.getElementById("geolocate-button")
+  let city
+  navigator.geolocation.getCurrentPosition((position) => {
     lat = parseFloat(position.coords.latitude)
     lon = parseFloat(position.coords.longitude)
+  })
 
-    fetch(`https://nominatim.openstreetmap.org/reverse?format=geojson&lat=${lat}&lon=${lon}`)
-    .then((response) => response.json())
-    .then((data) => {
-        address = data['features'][0]['properties']['address']['house_number'] + " " + data['features'][0]['properties']['address']['road'];
-    })
+  geolocateButton.addEventListener('click', (event) => {
+    fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`)
+      .then((response) => response.json())
+      .then((data) => {
+        wait(2)
+        address = blankIfNull(data['address']['house_number']) + " " + data['address']['road'];
+        cat_address.value = blankIfNull(data['address']['house_number']) + " " + data['address']['road'];
 
+
+      })
+      catLat.value = lat
+      catLon.value = lon
   });
 
+  cat_address.addEventListener("keyup", (event) => {
+    clearTimeout(timeout)
 
-  addEventListener("turbo:frame-load", (e) => {
-    
-    let catLat = document.getElementById("cat_latitude")   
-    let catLon = document.getElementById("cat_longitude")
-    let cat_address = document.getElementById('cat_address')
-     
-    catLat.value = lat
-    catLon.value = lon
-    cat_address.value = address
-    })
-    removeEventListener("turbo:frame-load", e)
+    timeout = setTimeout(function () {
+      fetch(`https://nominatim.openstreetmap.org/reverse?format=json&lat=${lat}&lon=${lon}`)
+        .then((response) => response.json())
+        .then((data) => {
+          city = data['address']['city']
+          fetch(`https://nominatim.openstreetmap.org/search?q=${cat_address.value.split(" ").join("+")}+${city}&format=json`)
+            .then((response) => response.json())
+            .then((data) => {
+              catLat.value = data[0]["lat"]
+              catLon.value = data[0]["lon"]
+            })
+        })
 
 
-    function wait(sec) {
-        const date = Date.now();
-        let currentDate = null;
-        do {
-          currentDate = Date.now();
-        } while (currentDate - date < sec*1000);
-      }
+    }, 1000);
+
+
+  })
+
+})
+
+removeEventListener("turbo:frame-load", e)
+
+
+function wait(sec) {
+  const date = Date.now();
+  let currentDate = null;
+  do {
+    currentDate = Date.now();
+  } while (currentDate - date < sec * 1000);
+}
+
+function blankIfNull(data) {
+  if (typeof data === 'undefined') {
+    return ''
+  } else {
+    return data
+  }
+}
