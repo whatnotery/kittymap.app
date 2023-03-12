263b893f3022dff933389bde5cea547a3ee62999
Jos O'shea <jos@joshea.dev>
2023-03-12 12:40:56 -0400

    more validations

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,10 +1,23 @@
 class Cat < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
 
+  validates :alias,
+  presence: true,
+  length: { minimum: 3},
+  uniqueness: { case_sensitive: false }
 
+  validates :description,
+  presence: true,
+  length: { minimum: 10 }
+
+  validates :latitude,
+  presence: true
+
+  validates :latitude,
+  presence: true
 end
 
 
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,9 @@
 class Comment < ApplicationRecord
   belongs_to :cat
   belongs_to :user
+
+  validates :body,
+  presence: true,
+  length: { minimum: 10 }
+
 end
